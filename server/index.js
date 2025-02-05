528641c27a9df083da434049d2ea357de860dc00
devanshtyagi26 <tyagidevansh3@gmail.com>
2025-02-05 19:20:56 +0530

    Fixed Cors Issue

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+// Enable CORS and allow requests from your frontend
+app.use(cors({ origin: 'https://happenings-now.vercel.app' }));
+
 app.use(express.json());
 
-const eventsRouter = require("./routes/events");
-app.use("/api/events", eventsRouter); // Use the events route
+const eventsRouter = require('./routes/events');
+app.use('/api/events', eventsRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
