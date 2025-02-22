57860d793a16cae1f22b7e2f7252adaf40d4a8f1
devanshtyagi26 <tyagidevansh3@gmail.com>
2025-02-22 17:54:41 +0530

    Card Commenitng

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+//? Card Component
+// This component renders event details including title, date, location, description, rating, and buttons for ticket booking and location access.
+// Implements authentication for ticket access via OTP verification.
+
+import React, { useState } from "react";
 import "../Style/card.css";
 import { DateTime } from "../assets/DateTime";
 import { CardImage } from "../assets/CardImage";
 import { Location } from "../assets/Location";
 import { StarRating } from "../assets/Star";
 import Event from "../utils/Otp";
-import { useState } from "react";
 
 function Card({
   title,
@@ -17,29 +20,36 @@ function Card({
   ticketLink,
   eventLocationLink,
 }) {
+  // Function to format the event date into a more readable format
   const cardDate = (data) => {
     let split = data.split(",");
     return split[1] + " | " + split[2];
   };
-  const [Auth, setAuth] = useState(false);
+
+  const [Auth, setAuth] = useState(false); // State to track authentication status
+
+  // Function to trigger authentication process
   const authenticate = () => {
     setAuth(true);
   };
+
   return (
     <>
       <div className="frame">
         <div className="canvas">
+          {/* Display event image */}
           <div className="svgImage">
             <CardImage image={imgUrl} />
           </div>
           <div className="content">
+            {/* Display event title and rating */}
             <div className="title">
               <p className="event">{title}</p>
               <div className="rating">
-                {console.log(rating)}
                 <StarRating rating={rating} />
               </div>
             </div>
+            {/* Display event date and location */}
             <div className="location">
               <div className="dateTime">
                 <DateTime />
@@ -50,13 +60,14 @@ function Card({
                 <p>{address[1]}</p>
               </div>
             </div>
+            {/* Display event description */}
             <div className="description">{description}</div>
+            {/* Ticket and location buttons */}
             <div className="buttons">
               <button className="button" onClick={authenticate}>
                 Ticket
               </button>
               {Auth && <Event target={ticketLink} />}
-
               <button
                 className="button"
                 onClick={() => window.open(eventLocationLink, "_blank")}
